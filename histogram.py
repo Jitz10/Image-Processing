9a483ee10ef1d8787e5d96552229aa7cab517be1
Jiten Ganwani <Jitz10.ganwanijiten@gmail.com>
2024-10-01 11:16:58 +0530

    Includes histogram equalization

---
+++ histogram.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read the image in grayscale
+img = cv2.imread('nyc1.jpg', cv2.IMREAD_GRAYSCALE)
+
+# Apply histogram equalization
+equalized_img = cv2.equalizeHist(img)
+
+# Display original and equalized images side by side using matplotlib
+plt.subplot(1, 2, 1)
+plt.imshow(img, cmap='gray')
+plt.title('Original Image')
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.imshow(equalized_img, cmap='gray')
+plt.title('Equalized Image')
+plt.axis('off')
+
+plt.show()
+
+# Save the equalized image
+cv2.imwrite('equalized_image.jpg', equalized_img)
